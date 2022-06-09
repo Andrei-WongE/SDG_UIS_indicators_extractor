d865e364b0cf0770858fa98b8f3a48a77d12247a
Andrei-WongE <44266405+Andrei-WongE@users.noreply.github.com>
2022-06-09 10:44:25 -0500

    Added sectiong of bundling UIS to GPE indicators
    1sta take, need to incorporate in mapply

--- Ref_indicators.R
+++ Ref_indicators.R
@@ -31,7 +31,7 @@ options(scipen = 100, digits = 4) # Prefer non-scientific notation
 if (!require("pacman")) {
   install.packages("pacman")
 }
-pacman::p_load(dplyr, tidyverse, tidylog, openxlsx, here, purrr)
+pacman::p_load(dplyr, tidyverse, tidylog, openxlsx, here, purrr, listviewer)
 
 ## Runs the following ----------------------------------------------------------
 
@@ -40,87 +40,89 @@ pacman::p_load(dplyr, tidyverse, tidylog, openxlsx, here, purrr)
 ## Loading databases -----------------------------------------------------------
 
 ind_ref <- readWorkbook(here("SDG_related_indicators", 
-                             "ref_indicators_UIS.xlsx"),
-                        colNames = TRUE,
-                        skipEmptyRows = TRUE,
-                        check.names = TRUE,
-                        fillMergedCells = FALSE
-                      ) |> 
-                        select(c(gpe, label, gpe_name))
-  
-  
-ind_db <-  readWorkbook(here("SDG_related_indicators", 
-                             "1.gpe_data_national_REPORT.xlsx"),
-                        colNames = TRUE,
-                        skipEmptyRows = TRUE,
-                        check.names = TRUE,
-                        fillMergedCells = FALSE
-                      ) |> 
-                        select(c(indicator_id, country_id, year, value, country_name_en, indicator_label_en))
-
-# pcfc_2020 <- readWorkbook(here("SDG_related_indicators", 
-#                                "df_unesco_gpe_2020.xlsx"),
-#                           colNames = TRUE,
-#                           skipEmptyRows = TRUE,
-#                           check.names = TRUE,
-#                           fillMergedCells = FALSE
-                        ) 
-  
-pcfc_2021 <- readWorkbook(here("SDG_related_indicators", 
-                               "df_unesco_gpe_2021.xlsx"),
+                               "ref_indicators_UIS.xlsx"),
                           colNames = TRUE,
                           skipEmptyRows = TRUE,
                           check.names = TRUE,
                           fillMergedCells = FALSE
-                        )
+                        ) |> 
+                          select(c(gpe, label, gpe_name))
+    
+    
+ind_db <-  readWorkbook(here("SDG_related_indicators", 
+                               "1.gpe_data_national_REPORT.xlsx"),
+                          colNames = TRUE,
+                          skipEmptyRows = TRUE,
+                          check.names = TRUE,
+                          fillMergedCells = FALSE
+                        ) |> 
+                          select(c(indicator_id, country_id, year, value, country_name_en, indicator_label_en))
+  
+  # pcfc_2020 <- readWorkbook(here("SDG_related_indicators", 
+  #                                "df_unesco_gpe_2020.xlsx"),
+  #                           colNames = TRUE,
+  #                           skipEmptyRows = TRUE,
+  #                           check.names = TRUE,
+  #                           fillMergedCells = FALSE
+  #                        ) 
+    
+pcfc_2021 <- readWorkbook(here("SDG_related_indicators", 
+                                 "df_unesco_gpe_2021.xlsx"),
+                            colNames = TRUE,
+                            skipEmptyRows = TRUE,
+                            check.names = TRUE,
+                            fillMergedCells = FALSE
+                          )
 
 ## Preparing data for function -------------------------------------------------
 
 db <- left_join(ind_db, ind_ref, by = c("indicator_label_en" = "label")) |>
-  filter(!(gpe %in% "GPE indicator 1")) |> 
-  rename("iso" = "country_id") |> 
-  mutate(gpe_new = substr(gpe, 1, 15)) |> 
-  select(!c(indicator_label_en, gpe_name, gpe))
+    filter(!(gpe %in% "GPE indicator 1")) |> 
+    rename("iso" = "country_id") |> 
+    select(!c(indicator_label_en, gpe_name))
+    
+  # Check list of gpe indicators
   
-#Check list of gpe indicators
-
-db |> distinct(gpe_new) |> 
-  count()   #17 indicators, as indicator 1 was dropped 
+db |> distinct(gpe) |> 
+    count()   # 28 UIS indicators, as indicator 1 was dropped 
   
 
-#NOTE: Using PCFC 2021 for reporting years 2020 and 2021, as per UIS directive.
+# NOTE: Using PCFC 2021 for reporting years 2020 and 2021 as per UIS directive.
 
 ## Implementing function -------------------------------------------------------
-pcfc <- pcfc_2021
 
-data <- db
-
-reporting_year <- 2021
-
-indicators <- unique(db$gpe_new) 
+  pcfc <- pcfc_2021
   
-
-subset_a <- c("GPE indicator 2",
-                "GPE indicator 3ia",
-                "GPE indicator 3ib",
-                "GPE indicator 7ia",
-                "GPE indicator 7ib",
-                "GPE indicator 7ic",
-                "GPE indicator 7id",
-                "GPE indicator 8i3",
-                "GPE indicator 8i4",
-                "GPE indicator 8i5",
-                "GPE indicator 8i6",
-                "GPE indicator 8i7",
-                "GPE indicator 8i8",
-                "GPE indicator 8i9",
-                "GPE indicator 8i10",
-                "GPE indicator 8i11",
-                "GPE indicator 8i12")
+  data <- db
+  
+  reporting_year <- 2020
+  
+  indicators <- unique(db$gpe)
+  listviewer::jsonedit(indicators) # Verifying that we have the right indicators
+  
+    
+  subset_a <- c("GPE indicator 2",
+                  "GPE indicator 3ia",
+                  "GPE indicator 3ib",
+                  "GPE indicator 7ia",
+                  "GPE indicator 7ib",
+                  "GPE indicator 7ic",
+                  "GPE indicator 7id",
+                  "GPE indicator 8i3",
+                  "GPE indicator 8i4",
+                  "GPE indicator 8i5",
+                  "GPE indicator 8i6",
+                  "GPE indicator 8i7",
+                  "GPE indicator 8i8",
+                  "GPE indicator 8i9",
+                  "GPE indicator 8i10",
+                  "GPE indicator 8i11",
+                  "GPE indicator 8i12")
 
   
-  lapply(indicators, function(i) {
-
+lapply(indicators, function(i) {
+  
+  # Setting-up conditions
   if (reporting_year == 2020 & i %in% subset_a) {
     subset_years <- c(2017:2019)
     # pcfc <- pcfc_2020
@@ -138,13 +140,16 @@ subset_a <- c("GPE indicator 2",
     # pcfc <- pcfc_2021
   }
   
-    print(subset_years)
-    
+  print(paste('Processing element', i, subset_years, Sys.time()))
+
+  # Subsetting by years and indicator
   DF <- data[data[,3] %in% subset_years,]
   DF <- data[data[,6] %in% i,]
+  
+  # Adding full country list
   DF <- left_join(pcfc, DF, by = "iso")
   
-  
+  # Generating data.frame list
   DF_list <-  DF |> 
     select(!c(country_name_en, code_un)) |>
     group_by(indicator_id) |> 
@@ -154,29 +159,45 @@ subset_a <- c("GPE indicator 2",
   ) |>
     group_map(~.x, .keep = TRUE)
   
-
-openxlsx::write.xlsx(DF_list, here("SDG_related_indicators", 
+  #Exporting each indicator to workbook
+  openxlsx::write.xlsx(DF_list, here("SDG_related_indicators","rf_indicators", 
                                    paste0(i, "_UIS_",reporting_year,".xlsx")))
   
   }
 )
 
+## Bundling UIS indicators into GPE indicators ---------------------------------
 
-# DF_list <-  DF |> 
-#   select(!c(country_name_en, gpe)) |>
-#   group_by(indicator_id) |> 
-#   pivot_wider(
-#               names_from = indicator_id,
-#               values_from = value
-#   ) 
-# 
-# 
-# assign(paste0("PCFC_compare_", i),
-#        PCFC_compare,
-#        envir = .GlobalEnv)  # saves to environment
+db2 <- db |> mutate(gpe_new = substr(gpe, 1, 15)) 
+  
+  # Bundling conditions
+    
+    #group_2 <- #All 2
+    
+    #group_3i <- c("GPE indicator 3ia"
+    #              ,"GPE indicator 3ib")
+    
+    #group_3ii <- c("GPE indicator 3iia"
+    #               ,"GPE indicator 3iib"
+    #               ,"GPE indicator 3iic")
+    
+    #group_6 <- #All 6
+    
+    #group_7 <- #All 7
+    
+    #group_8 <- #ALL 8
+  
+db2 <- db2 |>
+  mutate(gpe_new = case_when(gpe == "GPE indicator 3ia"  ~ "GPE indicator 3i"
+                            ,gpe == "GPE indicator 3iia" ~ "GPE indicator 3ii"
+                            ,gpe == "GPE indicator 3iib" ~ "GPE indicator 3ii"
+                            ,gpe == "GPE indicator 3iic" ~ "GPE indicator 3ii"
+                            ,gpe == "GPE indicator 3ib"  ~ "GPE indicator 3i"
+                            ,TRUE ~ gpe_new
+  ))
 
-
-
+bundle <- unique(db2$gpe_new) # 6 GPE indicators, as indicator 1 was dropped 
+listviewer::jsonedit(bundle) # Verifying that we have the right indicators
 
 
 
